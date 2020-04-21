7211fd8a3771e721124213e4a23f217ddb30ef01
kk1234498 <62284841+kk1234498@users.noreply.github.com>
2020-04-21 19:23:16 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -481,7 +481,10 @@ async def on_message(message):
                                    "듀얼장소 : 2채 투기장\n"
                                    "듀얼시간 : 7시30분부터\n\n"
                                    "듀얼당일 7시20분까지 미리 모여주셔야합니다.\n\n```", delete_after=60.0)
-
+    elif message.content == ("~부족듀얼"):
+        channel = 692324317953785867
+        await client.get_channel(int(channel)).send("부족듀얼신청합니다.\n" + message.author.display_name)
+        await message.channel.send("```신청이 완료되었습니다.```", delete_after=60.0)
     elif message.content.startswith('~'):
         await message.channel.send("```명령어가 존재하지않습니다.\n~명령어 <-를 입력하여 명령어를 확인해주세요.```", delete_after=60.0)
 
