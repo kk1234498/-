50fb64bd2ca3421094cfc2953c573879e2bc2dd8
kk1234498 <62284841+kk1234498@users.noreply.github.com>
2020-04-26 23:04:30 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,8 +14,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-
-     if message.content == ("~명령어"):
+    if message.content == ("~명령어"):
         embed = discord.Embed(title="명령어모음집", description="[편의성관련]\n"
                                                           "~홈피\n~돈페트\n~주사위\n~가위바위보\n~듀얼신청\n~노래봇명령어\n"
                                                           "~허환작\n~독뎀\n~환포퀘\n~강화\n~경험치테이블\n~합성재료\n~합성토템\n~페트푸드\n"
