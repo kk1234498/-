d0add602fdbcc1c934486629b2667087e7fa1e03
kk1234498 <62284841+kk1234498@users.noreply.github.com>
2020-05-02 16:07:32 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ async def on_message(message):
     
     if message.author == client.user:
         return
-    elif (!message.content.startsWith(prefix)) return;
+    if (message.content.startsWith(prefix)) return;
 
     elif message.content == ("~!명령어"):
         embed = discord.Embed(title="명령어모음집", description="[편의성관련]\n"
