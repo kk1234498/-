a5cf18117534e7467a181e868d47da1c2d3802fc
kk1234498 <62284841+kk1234498@users.noreply.github.com>
2021-04-15 21:40:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ async def on_message(message):
     if message.author == bot.user:
         return
    
+    elif message.content == '!수룡':
+        await message.channel.send('{0.author.mention} ```준비중```')  # 명령어 쓴사람 태그후 메시지 표시
   
 
 access_token = os.environ['BOT_TOKEN']
