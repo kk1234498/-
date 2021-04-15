0774ef84de4384e93245b91a71a78701ec032d55
kk1234498 <62284841+kk1234498@users.noreply.github.com>
2021-04-15 21:33:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,6 @@ async def on_ready():
     print(f'봇연결이 완료되었습니다. {bot.user}')
 
 
-
-
-#@bot.event
-#async def on_message_edit(before, after):
-	#await before.channel.send(before.content + "->" + after.content)
-
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
